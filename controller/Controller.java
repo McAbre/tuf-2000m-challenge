de2d7bce332871a8e6cf1975bea2b2258a144c02
Joel Andersson <joelande@abo.fi>
2017-04-07 21:14:56 +0300

    Added all values (I think). Some of them actually seem to work...

--- controller/Controller.java
+++ controller/Controller.java
@@ -57,21 +57,144 @@ public class Controller {
             //get timestamp
             String timestamp = data.split(",")[0];
             
+            //get flow rate
+            double flowRate = model.getReal4(data, 1, 2);
+            model.addItem(model.fixLength("Flow rate", flowRate+"", "m^3/h"));
+            
+            //get energy flow rate
+            double energyFlowRate = model.getReal4(data, 3, 4);
+            model.addItem(model.fixLength("Energy flow rate", energyFlowRate+"", "GJ/h"));
+            
+            //get velocity
+            double velocity = model.getReal4(data, 5, 6);
+            model.addItem(model.fixLength("Velocity", velocity+"", "m/s"));
+            
+            //get fluid sound speed
+            double fluidSoundSpeed = model.round(2, model.getReal4(data, 7, 8));
+            model.addItem(model.fixLength("Fluid sound speed", fluidSoundSpeed+"", "m/s"));
+            
             //get positive accumulator
-            //double posAcc = model.getPositiveAccumulator(data);
-            //model.addItem(model.fixLength("Positive accumulator", posAcc, "unit"))
+            double posAcc = model.getLong(data, 9, 10);
+            double posAccDecFrac = model.getReal4(data, 11, 12);
+            model.addItem(model.fixLength("Positive accumulator", model.round(2,(posAcc*posAccDecFrac))+"", "unit"));
             
-            //get signal value
+            //get negative accumulator
+            double negAcc = model.getLong(data, 13, 14);
+            double negAccDecFrac = model.getReal4(data, 15, 16);
+            model.addItem(model.fixLength("Negative accumulator", model.round(2, (negAcc*negAccDecFrac))+"", "unit"));
+            
+            //get positive energy accumulator
+            double posEnergyAcc = model.getLong(data, 17, 18);
+            double posNrgAccDecFrac = model.getReal4(data, 19, 20);
+            model.addItem(model.fixLength("Positive energy accumulator", (model.round(2, posNrgAccDecFrac*posEnergyAcc))+"", "J"));
+            
+            //get negative energy accumulator
+            double negEnergyAcc = model.getLong(data, 21, 22);
+            System.out.println("controller.Controller.negEnergyAcc(): "+negEnergyAcc);
+            double negNrgAccDecFrac = model.getReal4(data, 23, 24);
+            System.out.println("controller.Controller.negNrgAccDecFrac(): "+negNrgAccDecFrac);
+            model.addItem(model.fixLength("Negative energy accumulator", (model.round(2, negNrgAccDecFrac*negEnergyAcc))+"", "J"));
+            
+            //get net accumulator
+            double netAcc = model.getLong(data, 25, 26);
+            System.out.println("controller.Controller.netAcc(): "+netAcc);
+            double netDecFrac = model.getReal4(data, 27, 28);
+            System.out.println("controller.Controller.netDecFrac(): "+netDecFrac);
+            model.addItem(model.fixLength("Net accumulator", model.round(2, netAcc*netDecFrac)+"", "unit"));
+            
+            //get net energy accumulator
+            double netNrgAcc = model.getLong(data, 29, 30);
+            double netNrgDecFrac = model.getReal4(data, 31, 32);
+            model.addItem(model.fixLength("Net energy accumulator", model.round(2, netNrgAcc*netNrgDecFrac)+"", "J"));
+            
+            //get inlet temperature value
+            double inletTemp = model.round(2, model.getReal4(data, 33, 34));
+            model.addItem(model.fixLength("Inlet temperature", inletTemp+"", "C"));
+            
+            //get outlet temperature value
+            double outletTemp = model.round(2, model.getReal4(data, 35, 36));
+            model.addItem(model.fixLength("Outlet temperature", outletTemp+"", "C"));
+            
+            //get analog input AI3
+            double ai3 = model.round(2, model.getReal4(data, 37, 38));
+            model.addItem(model.fixLength("Analog input AI3", ai3+"", "unit"));
+            
+            //get analog input AI4
+            double ai4 = model.round(2,  model.getReal4(data, 39, 40));
+            model.addItem(model.fixLength("Analog input AI4", ai4+"", "unit"));
+            
+            //get analog input AI5
+            double ai5 = model.round(2, model.getReal4(data, 41, 42));
+            model.addItem(model.fixLength("Analog input AI5", ai5+"", "unit"));
+            
+            //get analog input AI5
+            double ai3current = model.round(2, model.getReal4(data, 43, 44));
+            model.addItem(model.fixLength("Current input AI3", ai3current+"", "mA"));
+            
+            //get analog input AI5
+            double ai4current = model.round(2, model.getReal4(data, 45, 46));
+            model.addItem(model.fixLength("Current input AI4", ai4current+"", "mA"));
+            
+            //get analog input AI5
+            double ai5current = model.round(2, model.getReal4(data, 47, 48));
+            model.addItem(model.fixLength("Current input AI5", ai5current+"", "mA"));
+            
+            //get error code
+            model.addItem(model.fixLength("Error Code", Integer.toBinaryString(Integer.parseInt(model.getRegister(data, 72))), ""));
+            
+            //get PT100 resistance of inlet
+            double inletResistance = model.round(2, model.getReal4(data, 77, 78));
+            model.addItem(model.fixLength("PT100 inlet resistance", inletResistance+"", "Ohm"));
+            
+            //get PT100 resistance of outlet
+            double outletResistance = model.round(2, model.getReal4(data, 79, 80));
+            model.addItem(model.fixLength("PT100 outlet resistance", outletResistance+"", "Ohm"));
+            
+            //get total travel time
+            double totalTravelTime = model.round(2, model.getReal4(data, 81, 82));
+            model.addItem(model.fixLength("Total travel time", totalTravelTime+"", "µs"));
+            
+            //get delta travel time
+            double deltaTravelTime = model.round(2, model.getReal4(data, 83, 84));
+            model.addItem(model.fixLength("Delta travel time", deltaTravelTime+"", "ns"));
+                    
+            //get upstream travel time
+            double upstreamTravelTime = model.round(2, model.getReal4(data, 85, 86));
+            model.addItem(model.fixLength("Upstream travel time", upstreamTravelTime+"", "µs"));
+            
+            //get downstream travel time
+            double downstreamTravelTime = model.round(2, model.getReal4(data, 87, 88));
+            model.addItem(model.fixLength("Downstream travel time", downstreamTravelTime+"", "µs"));
+            
+            //get output current
+            double outputCurrent = model.round(2, model.getReal4(data, 89, 90));
+            model.addItem(model.fixLength("Output current", outputCurrent+"", "mA"));
+            
+            //get working step
+            int workingStep = model.getWorkingStep(data);
+            model.addItem(model.fixLength("Working step", workingStep+"", "(0-99)"));
+            
+            //get signal quality
             int signal = model.getSignal(data);
-            model.addItem(model.fixLength("Signal quality", signal+"", "unit"));
+            model.addItem(model.fixLength("Signal quality", signal+"", "(0-99)"));
             
-            //get temperature value
-            double temperature = model.getTemperature(data);
-            model.addItem(model.fixLength("Temperature", temperature+"", "celcius"));
+            //get upstream strength
+            int upstreamStrength = Integer.parseInt(model.getRegister(data, 93));
+            model.addItem(model.fixLength("Upstream strength", upstreamStrength+"", "(0-2047)"));
             
-            //get negative energy value
-            double negEnergyAcc = model.getNegativeEnergy(data);
-            model.addItem(model.fixLength("Negative energy accumulator", negEnergyAcc+"", "unit"));
+            //get downstream strength
+            int downstreamStrength = Integer.parseInt(model.getRegister(data, 94));
+            model.addItem(model.fixLength("Downstream strength", downstreamStrength+"", "(0-2047)"));
+            
+            //get language used in user interface
+            String language = model.getLanguage(data);
+            model.addItem(model.fixLength("Language", language, ""));
+            
+            //get the rate of the measured travel time by the calculated travel time
+            
+            //get Reynolds number. At least ask him out to dinner first! ;D
+            double Reynold = model.round(2, model.getReal4(data, 99, 100));
+            model.addItem(model.fixLength("Reynolds number", Reynold+"", ""));;
             
             //update values
             System.out.println("timeLabel: " + timeLabel);
--- model/Model.java
+++ model/Model.java
@@ -53,10 +53,10 @@ public class Model {
     }
     
     public String fixLength(String type, String value, String unit){
-        int width = 320;
+        int width = 315;
         String spaces = "";
         
-        Font font = Font.getFont("Default");
+        Font font = new Font("Arial", Font.PLAIN, 14);
         FontMetrics metrics = new FontMetrics(font) {  
         };
         Rectangle2D bounds = metrics.getStringBounds(type + spaces + value + " " + unit, null);  
@@ -89,7 +89,7 @@ public class Model {
     }
     
     //Returns specific register from the data string as a string
-    private String getRegister(String data, int register){
+    public String getRegister(String data, int register){
         /**
          * Raw data as csv as such 1:26954, 2:31231, 3:1231231
          * Step 1: Split at "register:"
@@ -102,25 +102,49 @@ public class Model {
         return temp;
     }
     
-    private double round(int decimalPoints, double value){
+    public double round(int decimalPoints, double value){
         return BigDecimal.valueOf(value).setScale(decimalPoints, RoundingMode.CEILING).doubleValue();
+    }
+    
+    public double getLong(String data, int reg0, int reg){
+        String reg1 = getRegister(data, reg0);
+        String reg2 = getRegister(data, reg); 
+        return longParse(reg1, reg2);
+    }
+    public double getReal4(String data, int reg0, int reg){
+        String reg1 = getRegister(data, reg0);
+        String reg2 = getRegister(data, reg);
+        return magnitudeDecider(reg1, reg2);
+    }
+    
+    
+    public String getLanguage(String data){
+        int langNum = Integer.parseInt(getRegister(data, 96));
+        String language = "";
+        switch(langNum){
+            case 0: language  = "English"; break;
+            case 1: language  = "Chinese"; break;
+            default: language = "Error";
+        }
+        return language;
+    }
+    
+    public int getWorkingStep(String data){
+        String workingStep = getRegister(data, 92);
+        String temp = Long.toBinaryString(Integer.parseInt(workingStep));
+        while(temp.length() < 16){
+            temp = "0" + temp;
+        }
+        //We want the first half.
+        String workingBinary = temp.substring(0, temp.length()/2);
+        //2 stands for base 2
+        int workingSt = Integer.parseInt(workingBinary, 2);
+        return workingSt;
     }
     
     public int getSignal(String data) {
         String signal = getRegister(data, 92);
         return signalQuality(signal);
-    }
-
-    public double getTemperature(String data) {
-        String reg33 = getRegister(data, 33);
-        String reg34 = getRegister(data, 34);
-        return round(2, temperature(reg33, reg34));
-    }
-
-    public double getNegativeEnergy(String data) {
-        String reg21 = getRegister(data, 21);
-        String reg22 = getRegister(data, 22);
-        return negativeEnergy(reg21, reg22);
     }
     
     private int signalQuality(String value) {
@@ -128,7 +152,7 @@ public class Model {
         while(temp.length() < 16){
             temp = "0" + temp;
         }
-        //We want the latter half. Because modbus...
+        //We want the latter half.
         String signalBinary = temp.substring(temp.length()/2);
         //2 stands for base 2
         int signalQuality = Integer.parseInt(signalBinary, 2);
@@ -136,20 +160,20 @@ public class Model {
     }
     
     //Is this really right? Idk how else to get the correct value though.
-    private double negativeEnergy(String strReg21, String strReg22){
-        int reg21 = Integer.parseInt(strReg21);
-        int reg22 = Integer.parseInt(strReg22);
-        return (reg21-reg22-1);
+    private double longParse(String strReg1, String strReg2){
+        int reg1 = Integer.parseInt(strReg1);
+        int reg2 = Integer.parseInt(strReg2);
+        return (reg1-reg2-1);
     }
     
-    private double temperature(String reg33, String reg34){
+    private double real4(String reg1, String reg2){
         //-1^sign * 2^exponent * mantissa
         int sign;
         int exponent;
         double mantissa;
         
-        int highWord = Integer.parseInt(reg33);
-        int lowWord = Integer.parseInt(reg34);
+        int highWord = Integer.parseInt(reg1);
+        int lowWord = Integer.parseInt(reg1);
         
         String[] highNlow = {Integer.toBinaryString(highWord), Integer.toBinaryString(lowWord)};
         
@@ -183,13 +207,63 @@ public class Model {
             mantissa = mantissa/4194304;
         }
         
-        
-        double a = Math.pow(-1, sign);
-        double b = Math.pow(2, exponent);
-        double c = mantissa;
-        
-        return a*b*c;
+        //                      -1^sign   * 2^exponent            * mantissa
+        double real4 = Math.pow(-1, sign) * Math.pow(2, exponent) * mantissa;
+        return real4;
     }
     
+    //Calculates distance from 0 (think x*10 or x*10^-1) where the ^n is the magnitude
+    private int magnitude (double number){
+        int mag = 0;
+        
+        //Number is between 0 and 1 (this most likely means it looks like this 0.0000000000001 ish)
+        if(number < 1 && number > 0){
+            while(number < 1){
+                number = number * 10;
+                mag++;
+            }
+        //Number is greater than 1
+        }else if(number > 1){
+            while(number > 1){
+                number = number / 10;
+                mag++;
+            }
+        //Number is less than or equal to 0
+        }else{
+            number = number*(-1);//Flips number to be positive
+            //Number is less than 0 and greater than -1(before the flip to positive)
+            if(number < 1 && number > 0){
+                while(number < 1){
+                    number = number * 10;
+                    mag++;
+                }
+            }
+            //Number is less than or equal to -1 (before the flip to positive)
+            else{
+                while(number > 1){
+                    number = number / 10;
+                    mag++;
+                }
+            }
+            
+        }
+        
+        return mag;
+    }
+    private double magnitudeDecider(String reg1, String reg2){
+        double result;
+        //Reality check... Maybe the temperature isn't 0.0000000001C or a few million C
+        double val1 = real4(reg1, reg2);
+        double val2 = real4(reg2, reg1);
+        int mag1 = magnitude(val1);
+        int mag2 = magnitude(val2);
+        System.out.println("magnitude("+reg1+","+reg2+"): " + "val1="+val1+" val2="+val2 + " mag= " + mag1 + " | " + mag2);
+        if(mag1 < mag2 && mag1 != 0 || mag1 == 0 && mag2 > 12){
+            result = val1;
+        }else{
+            result = val2;
+        }
+        return result;
+    }
     
 }
